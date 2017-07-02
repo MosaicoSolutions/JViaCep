121e4330f461269f93413daad0bb7f007c447ee8
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-07-02 13:38:14 -0300

    Adição da interface EnderecoConvert.

---
+++ src/main/java/br/com/mosaicosolutions/jviacep/util/EnderecoConvert.java
@@ -0,0 +1,16 @@
+package br.com.mosaicosolutions.jviacep.util;
+
+import java.util.Optional;
+
+/**
+ * Fornece métodos para conversões entre Json e Endereço.
+ *
+ * @author BrunoXavier
+ * @version 1.0
+ */
+public interface EnderecoConvert {
+
+    //Optional<Endereco> deJsonParaEndereco(String json);
+
+    //Stream<Endereco> deJsonParaListaDeEndereco(String json);
+}
